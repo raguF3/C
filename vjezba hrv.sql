8172d168788117905a35a4da9c34e116475f2024
Raguž Lea <raguz.leaa@gmail.com>
2024-05-22 23:26:10 +0200

    Create vjezba hrv.sql

---
+++ vjezba hrv.sql
@@ -0,0 +1,38 @@
+﻿--create database hrv;
+--go
+--use hrv;
+
+--create table zupanije(
+--sifra int not null primary key identity (1,1),
+--naziv varchar (50) not null,
+--zupan int
+--);
+
+
+--create table zupani (
+--sifra int not null primary key identity (1,1),
+--ime varchar (50) not null,
+--prezime varchar (50) not null
+--);
+
+
+--create table opcine (
+--sifra int not null primary key identity (1,1),
+--zupanija int,
+--naziv varchar (50) not null
+--);
+
+--create table mjesta (
+--sifra int not null primary key identity (1,1),
+--opcina int,
+--naziv varchar (50) not null
+--);
+
+
+alter table opcine add foreign key (zupanija) references zupanije (sifra);
+alter table mjesta add foreign key (opcina) references opcine (sifra);
+alter table zupanije add foreign key (zupan) references zupani (sifra);
+
+
+select * from opcine;
+
