0202d1849b088e5855cb310b23a8f7fd2fc921d5
Raguž Lea <raguz.leaa@gmail.com>
2024-05-27 21:43:04 +0200

    update radnikEraaltertable

--- racunEraaltertable.sql
+++ racunEraaltertable.sql
@@ -1,27 +1,43 @@
-﻿use master;
-go
-drop database if exists racunEra;
-go
-create database racunEra;
-go
-use racunEra;
+﻿--use master;
+--go
+--drop database if exists racunEra;
+--go
+--create database racunEra;
+--go
+--use racunEra;
 
-create table racuni (
+--create table racuni (
+--sifra int not null primary key identity (1,1),
+--redniBroj int,
+--datum datetime,
+--kupac varchar (50),
+--placno bit
+--);
+
+--create table stavke_racuna (
+--racun int,
+--artikl int,
+--cijena decimal (18,2),
+--kolicina int,
+--popust int
+--);
+
+--alter table stavke_racuna add foreign key (racun) references racuni (sifra);
+
+
+use master;
+go
+drop database if exists radnikEra;
+go
+create database radnikEra;
+go
+use radnikEra;
+
+create table radnik (
 sifra int not null primary key identity (1,1),
-redniBroj int,
-datum datetime,
-kupac varchar (50),
-placno bit
+ime nvarchar (50),
+prezime nvarchar (50),
+nadredeni int
 );
 
-create table stavke_racuna (
-racun int,
-artikl int,
-cijena decimal (18,2),
-kolicina int,
-popust int
-);
-
-alter table stavke_racuna add foreign key (racun) references racuni (sifra);
-
-
+alter table radnik add foreign key (nadredeni) references radnik (sifra);
